cfda1a615b07131d745a20cadddd3b23bbe0095d
soro <sorolj@ept.sn>
2025-06-28 21:37:28 +0000

    le bon

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Spacehackers2Config(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'spacehackers_2'
+    name = 'users'
